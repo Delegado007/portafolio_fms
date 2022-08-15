17aab0d40f3c8cee8684bcdce23f2e9c2f395eb2
unknown <maxharper2013@gmail.com>
2022-08-15 12:55:18 -0300

    [ADD] grid project template

---
+++ src/component/ProjectsGrid/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Grid } from "./styles";
+
+
+const ProjectsGrid = () => {
+    return (
+        <Grid>
+            <div style={{backgroundColor: "white", gridArea: 1 / 1 / 2 / 2}}>Hola</div>
+        </Grid>
+    )
+}
