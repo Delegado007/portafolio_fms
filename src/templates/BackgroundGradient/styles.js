38566932002a04e06db367dd8060d8db4ef65605
Delegado <maxharper2013@gmail.com>
2022-08-31 13:08:59 -0300

    [FIX] viewport roto

--- src/templates/BackgroundGradient/styles.js
+++ src/templates/BackgroundGradient/styles.js
@@ -2,7 +2,5 @@ import styled from "styled-components";
 
 export const ContainerBG = styled.div`
   width: 100%;
-  min-width: 500px;
-  min-height: 1000px;
   height: 100%;
   background-image: linear-gradient(to bottom, #1d1d1d, #1a1b1e, #121b1f, #091a1d, #001a17, #011a17, #011b17, #021b17, #0a1d1e, #131f22, #1b2124, #222325);  `
