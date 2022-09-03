14f7687c8dc0f69e0f4695e76495517852112497
Delegado <maxharper2013@gmail.com>
2022-09-03 18:26:41 -0300

    [ADD] section resume

--- src/components/MenuVertical/index.js
+++ src/components/MenuVertical/index.js
@@ -33,6 +33,11 @@ export const MenuVertical = ({ open }) => {
             <Span  >Contact me</Span>
           </Li>
         </a>
+        <a href="https://drive.google.com/file/d/1A6Zuh8nJFGPbIBI-aTTPKZdIi-P7-x_E/view" target="_blank" onClick={handleSetOpen}>
+          <Li>
+            <Span  >Resume</Span>
+          </Li>
+        </a>
       </Lista>
       <LinksSVG>
         <GitHub />
