b4ddcc3097e1b3b042c33448693ffaf72c99bea2
Delegado <maxharper2013@gmail.com>
2022-09-09 08:40:31 -0300

    [ADD] restaurado time setTimeout

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -21,7 +21,7 @@ export const App = () => {
     setDataProjects(jsonDataProjects.projects);
     setTimeout(() => {
       dispatch(setLoading(false))
-    }, 1)
+    }, 1800)
   }, []);
 
   return (
