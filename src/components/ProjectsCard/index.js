4b66eb874694f04878a86da4863cc015ee9b28a7
Delegado <maxharper2013@gmail.com>
2022-09-09 14:22:37 -0300

    [FIX] quitando animacion en card

--- src/components/ProjectsCard/index.js
+++ src/components/ProjectsCard/index.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Card, CardBackground, CardContent, CardFooter, Button, CardOverlay, ButtonDeploy, ButtonCode, ContainerGridIconSVG } from './styles'
-import { useNearScreen } from '@hooks/useNearScreen';
 
 export const ProjectCard = ({ id, heading, background, resume, urlSite, urlCode, stack }) => {
   const code = "assets/code.png";
   const rocket = "assets/rocket.png";
 
   const [indexImg, setIndexImg] = useState(0);
-  const [show, element] = useNearScreen()
 
   useEffect(() => {
     const firstButtons = document.querySelector(`.button_change_img_${id}`);
@@ -31,42 +29,42 @@ export const ProjectCard = ({ id, heading, background, resume, urlSite, urlCode,
   }
 
   return (
-    <div ref={element}>
-      {show &&
-        <Card className="card animate__fadeInUp">
-          <CardBackground className="card__background" >
-            <img src={background[indexImg]}></img>
-          </CardBackground>
-          <CardOverlay className="card__overlay" />
-          <CardContent className="card__content" >
-            <h1>{heading}</h1>
-            <p>{resume}</p>
-            <ContainerGridIconSVG>
-              {stack.map((technology) => {
-                return (
-                  <div key={technology}>
-                    <img src={technology} alt="technology used" />
-                  </div>
-                )
-              })}
-            </ContainerGridIconSVG>
-          </CardContent>
-          <CardFooter className="card__footer">
-            {background.map((img, index) => {
+    <div>
+
+      <Card className="card animate__fadeInUp">
+        <CardBackground className="card__background" >
+          <img src={background[indexImg]}></img>
+        </CardBackground>
+        <CardOverlay className="card__overlay" />
+        <CardContent className="card__content" >
+          <h1>{heading}</h1>
+          <p>{resume}</p>
+          <ContainerGridIconSVG>
+            {stack.map((technology) => {
               return (
-                <div key={index}>
-                  <Button className={`button_change_img_${id}`} onClick={() => handleImgChange(index)}>
-                    {index + 1}
-                  </Button>
+                <div key={technology}>
+                  <img src={technology} alt="technology used" />
                 </div>
               )
             })}
-          </CardFooter>
-          <ButtonCode onClick={() => { window.open(`${urlCode}`, '_blank') }} ><img src={code}></img></ButtonCode>
-          <ButtonDeploy onClick={() => { window.open(`${urlSite}`, '_blank') }} ><img src={rocket}></img></ButtonDeploy>
-        </Card>
+          </ContainerGridIconSVG>
+        </CardContent>
+        <CardFooter className="card__footer">
+          {background.map((img, index) => {
+            return (
+              <div key={index}>
+                <Button className={`button_change_img_${id}`} onClick={() => handleImgChange(index)}>
+                  {index + 1}
+                </Button>
+              </div>
+            )
+          })}
+        </CardFooter>
+        <ButtonCode onClick={() => { window.open(`${urlCode}`, '_blank') }} ><img src={code}></img></ButtonCode>
+        <ButtonDeploy onClick={() => { window.open(`${urlSite}`, '_blank') }} ><img src={rocket}></img></ButtonDeploy>
+      </Card>
 
-      }
+
     </div>
   )
 }
--- src/components/ProjectsCard/styles.js
+++ src/components/ProjectsCard/styles.js
@@ -28,8 +28,6 @@ export const Card = styled.div`
   -webkit-box-shadow: 10px 10px 39px 10px rgba(0,0,0,1);
   -moz-box-shadow: 10px 10px 39px 10px rgba(0,0,0,1);
   box-shadow: 10px 10px 39px 10px rgba(0,0,0,1);
-  animation: ${jackInTheBox};
-  animation-duration: 1.1s;  
   &:hover .card__background{
   /* transform: scale(1.03) translateZ(0); */
   filter: brightness(1) contrast(1);  
